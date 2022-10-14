3bed2e05cee93f1b8be23f993793d90485e53c6a
Carlo Nicora <carlo.nicora@gmail.com>
2022-10-14 10:45:08 +0200

    feat: source local images from recursive folders

--- src/components/abstracts/AbstractComponentData.ts
+++ src/components/abstracts/AbstractComponentData.ts
@@ -2,7 +2,7 @@ import {AbstractRpgManager} from "../../abstracts/AbstractRpgManager";
 import {ComponentDataInterface} from "../interfaces/ComponentDataInterface";
 import {CampaignInterface} from "../components/campaign/interfaces/CampaignInterface";
 import {ComponentType} from "../enums/ComponentType";
-import {App, TFile} from "obsidian";
+import {App, TAbstractFile, TFile, TFolder} from "obsidian";
 import {CampaignSetting} from "../components/campaign/enums/CampaignSetting";
 import {IdInterface} from "../../id/interfaces/IdInterface";
 import {ComponentMetadataInterface} from "../interfaces/ComponentMetadataInterface";
@@ -10,6 +10,7 @@ import {ComponentStage} from "../enums/ComponentStage";
 
 export abstract class AbstractComponentData extends AbstractRpgManager implements ComponentDataInterface {
 	private static root: string|undefined;
+	private static _imageExtensions: Array<string> = ["jpeg", "jpg", "png", "webp"];
 
 	public static initialiseRoots(
 		app: App,
@@ -47,22 +48,7 @@ export abstract class AbstractComponentData extends AbstractRpgManager implement
 	public get image(): string | undefined {
 		if (this.metadata?.data?.image != undefined && this.metadata?.data?.image !== '') return this.metadata.data.image;
 
-		let localImage: string|undefined = undefined;
-		const imageExtensions = ["jpeg", "jpg", "png", "webp"];
-
-		for (let extensionCount = 0; extensionCount < imageExtensions.length; extensionCount++) {
-			const fileName = ((this.settings.imagesFolder !== undefined && this.settings.imagesFolder !== '') ? this.settings.imagesFolder : this.app.vault.config.attachmentFolderPath) + '/' + this.file.basename + '.' + imageExtensions[extensionCount];
-
-			if (this._fileExists(fileName)) {
-				if (AbstractComponentData.root === undefined) AbstractComponentData.initialiseRoots(this.app);
-				localImage = AbstractComponentData.root + fileName;
-				break;
-			}
-		}
-
-		if (localImage !== undefined) return localImage;
-
-		return undefined;
+		return this._getImage(this.file.basename);
 	}
 
 	public get isComplete(): boolean {
@@ -72,28 +58,42 @@ export abstract class AbstractComponentData extends AbstractRpgManager implement
 	abstract touch(
 	): boolean;
 
-	private _fileExists(
-		path: string
-	): boolean {
-		let folder: string|undefined = undefined;
-		const folderSeparatorIndex = path.lastIndexOf('/');
+	private _getImage(
+		basename: string
+	): string|undefined {
+		const folder = (this.settings.imagesFolder !== undefined && this.settings.imagesFolder !== '') ? this.settings.imagesFolder : this.app.vault.config.attachmentFolderPath;
+		const initialFolder = this.app.vault.getAbstractFileByPath(folder);
 
-		if (folderSeparatorIndex !== -1){
-			folder = path.substring(0, folderSeparatorIndex);
+		if (initialFolder == undefined || !(initialFolder instanceof TFolder))
+			return undefined;
+
+		const response = this._getImageFromFolder(basename, initialFolder);
+		if (response === undefined)
+			return undefined;
+
+		if (AbstractComponentData.root === undefined)
+			AbstractComponentData.initialiseRoots(this.app);
+
+		return  AbstractComponentData.root + response;
+	}
+
+	private _getImageFromFolder(
+		basename: string,
+		folder: TFolder,
+	): string|undefined {
+		const filesInFolder = this.app.vault.getFiles().filter((file: TFile) => file.parent === folder && file.basename.toLowerCase() === basename.toLowerCase() && AbstractComponentData._imageExtensions.includes(file.extension));
+		if (filesInFolder.length !== 0)
+			return filesInFolder[0].path;
+
+		const subFolders = folder.children.filter((file: TAbstractFile) => file instanceof TFolder);
+		for (let index=0; index<subFolders.length; index++){
+			const subFolder = subFolders[index]
+			if (subFolder instanceof TFolder) {
+				const image: string|undefined = this._getImageFromFolder(basename, subFolder);
+				if (image !== undefined) return image;
+			}
 		}
 
-		const allFiles = this.app.vault.getFiles().filter((file: TFile) =>
-			(
-				folder !== undefined
-					? file.path.substring(0, file.path.lastIndexOf('/')) === folder
-					: file.parent.parent === undefined
-			)
-		);
-
-		for (let index=0; index<allFiles.length; index++){
-			if (allFiles[index].path.toLowerCase() === path.toLowerCase()) return true
-		}
-
-		return false;
+		return undefined;
 	}
 }
