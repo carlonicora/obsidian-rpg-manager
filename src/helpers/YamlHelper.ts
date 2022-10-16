8eff4d85e0412d0b2e233dd90e78d667eba0e930
Carlo Nicora <carlo.nicora@gmail.com>
2022-10-17 01:17:51 +0200

    wip: rewrite YAML manager

--- src/helpers/YamlHelper.ts
+++ src/helpers/YamlHelper.ts
@@ -19,23 +19,11 @@ export class YamlHelper {
 
 		if (isArray) {
 			for (let index=0; index<yaml.length; index++){
-				response.push(...this._stringify(yaml[index], 0, false, true));
-			}
-		} else {
-			Object.entries(yaml).forEach(([key, value], index) => {
-				const yamlKey = isArrayChild ? '' : '  '.repeat(indent) + key + ': ';
-
-				if (Array.isArray(value)) {
-					const arrayReponse: Array<string> = this._stringify(value, 0, true);
-					for (let index=0; index<arrayReponse.length; index++){
-						if (index === 0){
-							arrayReponse[index] = '  '.repeat(indent) +  '- ' + arrayReponse[index];
-						} else {
-							arrayReponse[index] = '  '.repeat(indent + 1) + arrayReponse[index];
-						}
-					}
-					arrayReponse[0] = ' -' + arrayReponse[0].substring(2);
-					response.push(...arrayReponse);
+				const value: any = yaml[index];
+				if (value == null){
+					response.push('');
+				} else if (Array.isArray(value)){
+					response.push()
 				} else {
 					switch (typeof value) {
 						case "object":
@@ -43,20 +31,73 @@ export class YamlHelper {
 							break;
 						case "number":
 						case "boolean":
-							response.push(yamlKey + value);
+							response.push(value.toString());
 							break;
 						case "undefined":
+							break;
+						default:
+							response.push('"' + value + '"');
+							break;
+					}
+				}
+			}
+		} else {
+			Object.entries(yaml).forEach(([key, value], index) => {
+				const yamlKey = isArrayChild ? '' : '  '.repeat(indent) + key + ': ';
+
+				if (value == null){
+					response.push(yamlKey);
+				} else if (Array.isArray(value)) {
+					const isBasicArray = this._isBasicArray(value);
+
+					if (isBasicArray) {
+						const basicArrayReponse: Array<string> = this._stringify(value, 0, true);
+						response.push(yamlKey + '[' + basicArrayReponse.join(',') + ']');
+					} else {
+						response.push(yamlKey);
+						for (let index=0; index<value.length; index++){
+							const arrayReponse: Array<any> = this._stringify(value[index], 0, false)
+							for (let responseIndex=0; responseIndex<arrayReponse.length; responseIndex++) {
+								if (responseIndex === 0) {
+									arrayReponse[responseIndex] = '  '.repeat(indent + 1) + '- ' + arrayReponse[responseIndex];
+								} else {
+									arrayReponse[responseIndex] = '  '.repeat(indent + 2) + arrayReponse[responseIndex];
+								}
+							}
+
+							response.push(...arrayReponse);
+						}
+					}
+				} else {
+					switch (typeof value) {
+						case "object":
+							response.push(yamlKey);
+							response.push(...this._stringify(value, indent + 1));
+							break;
+						case "number":
+						case "boolean":
+							response.push(yamlKey + value);
 							break;
 						default:
 							response.push(yamlKey + '"' + value + '"');
 							break;
 					}
 				}
-
-				console.log(key, value, index);
 			});
 		}
 
 		return response;
+	}
+
+	private static _isBasicArray(
+		values: Array<any>,
+	): boolean {
+		for (let index=0; index<values.length; index++){
+			if (typeof values[index] === 'object')
+				return false;
+
+		}
+
+		return true;
 	}
 }
