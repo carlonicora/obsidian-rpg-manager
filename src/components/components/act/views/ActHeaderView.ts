82bec4d49bf7971573a506fbc62c8b075a940a28
Carlo Nicora <carlo.nicora@gmail.com>
2022-10-18 22:11:02 +0200

    fix: unused code removed

--- src/components/components/act/views/ActHeaderView.ts
+++ src/components/components/act/views/ActHeaderView.ts
@@ -4,7 +4,6 @@ import {HeaderResponseType} from "../../../../responses/enums/HeaderResponseType
 import {AbstractPlotHeaderView} from "../../../../views/abstracts/AbstractPlotHeaderView";
 import {HeadlessTableView} from "../../../../views/HeadlessTableView";
 import {ActInterface} from "../interfaces/ActInterface";
-import {SceneBuilderModal} from "../../../../modals/SceneBuilderModal";
 import {AnalyserInterface} from "../../../../analyser/interfaces/AnalyserInterface";
 import {AnalyserReportType} from "../../../../analyser/enums/AnalyserReportType";
 
