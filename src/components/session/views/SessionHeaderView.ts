28cda84baec777fa303c90395b1800bc54fdf04b
Carlo Nicora <carlo.nicora@gmail.com>
2022-10-30 14:20:19 +0100

    wip: refactor API

--- src/components/session/views/SessionHeaderView.ts
+++ src/components/session/views/SessionHeaderView.ts
@@ -3,6 +3,8 @@ import {NewHeaderViewInterface} from "../../../managers/viewsManager/interfaces/
 import {SessionInterface} from "../interfaces/SessionInterface";
 import {LongTextElement} from "../../../managers/viewsManager/elements/LongTextElement";
 import {AbtStageElement} from "../../../services/plotsServices/views/elements/AbtStageElement";
+import {DateElement} from "../../../services/dateService/views/elements/DateElement";
+import {TimeElement} from "../../../services/dateService/views/elements/TimeElement";
 
 export class SessionHeaderView extends AbstractHeaderView implements NewHeaderViewInterface {
 	public model: SessionInterface;
@@ -15,7 +17,12 @@ export class SessionHeaderView extends AbstractHeaderView implements NewHeaderVi
 		this.addGallery();
 		this.addInfoElement(LongTextElement, {title: 'Description', values: this.model.synopsis ?? '<span class="missing">Synopsis Missing</span>', editableKey: 'data.synopsis'});
 
+		this.addInfoElement(DateElement, {title: 'Session Date', values: this.model.irl, editableKey: 'data.irl'});
+
 		if (this.api.settings.usePlotStructures)
 			this.addInfoElement(AbtStageElement, {title: 'ABT Stage', values: this.model.abtStage, editableKey: 'data.abtStage'});
+
+		if (this.api.settings.useSceneAnalyser)
+			this.addInfoElement(TimeElement, {title: 'Target Duration', values: this.model.targetDuration, editableKey: 'data.targetDuration'});
 	}
 }
---
+++ src/services/dateService/views/elements/interfaces/TimeElementDataInterface.ts
@@ -0,0 +1,5 @@
+import {ElementDataInterface} from "../../../../../managers/viewsManager/interfaces/ElementDataInterface";
+
+export interface TimeElementDataInterface extends ElementDataInterface {
+	values: number|undefined,
+}
