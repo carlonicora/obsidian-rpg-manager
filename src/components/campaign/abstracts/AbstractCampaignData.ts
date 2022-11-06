8cc7e20b9dcbd84633d545fa21a6110386f39ed4
Carlo Nicora <carlo.nicora@gmail.com>
2022-11-06 10:39:19 +0100

    fix: solved current elements in campaign

--- src/components/campaign/abstracts/AbstractCampaignData.ts
+++ src/components/campaign/abstracts/AbstractCampaignData.ts
@@ -41,9 +41,8 @@ export abstract class AbstractCampaignData extends PlotsAbtOnly implements Campa
 			response = this.api.service(IdService).createFromID(this.metadata.data.currentAdventureId);
 		} catch (e) {
 			if (this.metadata.data.currentAdventureId.indexOf('-') === -1){
-				const [type, campaignId, adventureId] = this.metadata.data.currentAdventureId.split('/');
+				const [, campaignId, adventureId] = this.metadata.data.currentAdventureId.split('/');
 				response = this.api.service(IdService).create(ComponentType.Adventure, campaignId, adventureId);
-				console.warn(response.stringID);
 			}
 		}
 
@@ -59,7 +58,7 @@ export abstract class AbstractCampaignData extends PlotsAbtOnly implements Campa
 			response = this.api.service(IdService).createFromID(this.metadata.data.currentActId);
 		} catch (e) {
 			if (this.metadata.data.currentAdventureId.indexOf('-') === -1){
-				const [type, campaignId, adventureId, actId] = this.metadata.data.currentActId.split('/');
+				const [, campaignId, adventureId, actId] = this.metadata.data.currentActId.split('/');
 				response = this.api.service(IdService).create(ComponentType.Act, campaignId, adventureId, actId);
 			}
 		}
@@ -76,7 +75,7 @@ export abstract class AbstractCampaignData extends PlotsAbtOnly implements Campa
 			response = this.api.service(IdService).createFromID(this.metadata.data.currentSessionId);
 		} catch (e) {
 			if (this.metadata.data.currentSessionId.indexOf('-') === -1){
-				const [type, campaignId, sessionId] = this.metadata.data.currentSessionId.split('/');
+				const [, campaignId, sessionId] = this.metadata.data.currentSessionId.split('/');
 				response = this.api.service(IdService).create(ComponentType.Session, campaignId, undefined, undefined, undefined, sessionId);
 			}
 		}
