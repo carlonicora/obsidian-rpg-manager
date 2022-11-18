764b799f15b3e6f89bd7315b9c247b611b99181e
Carlo Nicora <carlo.nicora@gmail.com>
2022-11-18 19:08:08 +0000

    fix: updater to write correct ID

--- src/main.ts
+++ src/main.ts
@@ -83,7 +83,9 @@ export default class RpgManager extends Plugin implements RpgManagerInterface{
 			}
 		}
 
-		if (!requiresUpdate) this.initialise();
+		if (!requiresUpdate)
+			this.initialise();
+
 	}
 
 	public async initialise(
