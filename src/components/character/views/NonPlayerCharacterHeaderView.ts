47568b2ac79c9d3e773ff49b637ecd3cc4af9b5c
Carlo Nicora <carlo.nicora@gmail.com>
2022-12-02 22:00:26 +0100

    feat: add pronoun selection to character header

--- src/components/character/views/NonPlayerCharacterHeaderView.ts
+++ src/components/character/views/NonPlayerCharacterHeaderView.ts
@@ -8,25 +8,54 @@ import {DateElement} from "../../../services/dateService/views/elements/DateElem
 import {CalendarType} from "../../../services/dateService/enums/CalendarType";
 import {FantasyCalendarElement} from "../../../services/fantasyCalendarService/views/elements/FantasyCalendarElement";
 import {FantasyCalendarCategory} from "../../../services/fantasyCalendarService/enums/FantasyCalendarCategory";
+import {PronounElement} from "./elements/PronounElement";
 
 export class NonPlayerCharacterHeaderView extends AbstractHeaderView implements NewHeaderViewInterface {
 	public model: CharacterInterface;
 
-	public render(
-	): void {
+	public render(): void {
 		this.addBreadcrumb();
 		this.addTitle();
 		this.addComponentOptions();
 		this.addGallery();
 
-		this.addInfoElement(LongTextElement, {model: this.model, title: 'Description', values: this.model.synopsis ?? '<span class="missing">Synopsis Missing</span>', editableKey: 'data.synopsis'});
-		this.addInfoElement(LongTextElement, {model: this.model, title: 'Goals', values: this.model.goals ?? '<span class="missing">Goals Missing</span>', editableKey: 'data.goals'});
+		this.addInfoElement(LongTextElement, {
+			model: this.model,
+			title: 'Description',
+			values: this.model.synopsis ?? '<span class="missing">Synopsis Missing</span>',
+			editableKey: 'data.synopsis'
+		});
+		this.addInfoElement(LongTextElement, {
+			model: this.model,
+			title: 'Goals',
+			values: this.model.goals ?? '<span class="missing">Goals Missing</span>',
+			editableKey: 'data.goals'
+		});
+
+		this.addInfoElement(PronounElement, {
+			model: this.model,
+			title: 'Pronoun',
+			values: this.model.pronoun,
+			editableKey: 'data.pronoun',
+		});
 
 		if (this.model.age !== undefined)
 			this.addInfoElement(ShortTextElement, {model: this.model, title: 'Age', values: this.model.age.toString()});
 
-		this.addInfoElement(this.model.campaign.calendar === CalendarType.Gregorian ? DateElement : FantasyCalendarElement, {model: this.model, title: 'Date of Birth', values: this.model.dob, category: FantasyCalendarCategory.Birth, editableKey: 'data.dob'});
-		this.addInfoElement(this.model.campaign.calendar === CalendarType.Gregorian ? DateElement : FantasyCalendarElement, {model: this.model, title: 'Date of Death', values: this.model.death, category: FantasyCalendarCategory.Death, editableKey: 'data.death'});
+		this.addInfoElement(this.model.campaign.calendar === CalendarType.Gregorian ? DateElement : FantasyCalendarElement, {
+			model: this.model,
+			title: 'Date of Birth',
+			values: this.model.dob,
+			category: FantasyCalendarCategory.Birth,
+			editableKey: 'data.dob'
+		});
+		this.addInfoElement(this.model.campaign.calendar === CalendarType.Gregorian ? DateElement : FantasyCalendarElement, {
+			model: this.model,
+			title: 'Date of Death',
+			values: this.model.death,
+			category: FantasyCalendarCategory.Death,
+			editableKey: 'data.death'
+		});
 
 		if (this.model.death != null) {
 			let death = this.api.service(DateService).getReadableDate(this.model.death, this.model);
