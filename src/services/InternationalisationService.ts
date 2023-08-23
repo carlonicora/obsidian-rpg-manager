2c14f45634ba2a1c49bac9a683377cc8fac14a7c
Carlo Nicora <carlo.nicora@gmail.com>
2023-08-23 10:02:34 +0200

    fix: move i18n files

--- src/services/InternationalisationService.ts
+++ src/services/InternationalisationService.ts
@@ -1,7 +1,7 @@
 import i18n from "i18next";
 import I18nextBrowserLanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
-import { i18nEng } from "src/i18n/eng/eng";
+import { i18nEng } from "src/i18n/eng";
 
 export class InternationalisationService {
 	static async loadSettings(): Promise<void> {
