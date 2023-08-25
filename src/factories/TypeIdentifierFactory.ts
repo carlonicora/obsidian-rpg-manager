c24523ca0fe4c5340df97c61255e332c415e12dc
Carlo Nicora <carlo.nicora@gmail.com>
2023-08-25 12:31:31 +0200

    fix: remove unused component

--- src/factories/TypeIdentifierFactory.ts
+++
@@ -1,8 +0,0 @@
-import { ElementType } from "react";
-import { SystemType } from "src/data/enums/SystemType";
-
-export class TypeIdentifierFactory {
-	static generateTypeIdentifier(type: ElementType, system: SystemType) {
-		return type.toString() + "-" + system.toString();
-	}
-}
