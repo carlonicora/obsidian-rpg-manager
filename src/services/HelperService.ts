4fa4e3ec2b790957f98145db1c7c76596bf5272c
Carlo Nicora <carlo.nicora@gmail.com>
2023-08-26 21:41:28 +0200

    feat: move getPositionInParent to HelperService

--- src/services/HelperService.ts
+++ src/services/HelperService.ts
@@ -1,3 +1,5 @@
+import { ElementInterface } from "@/data/interfaces/ElementInterface";
+
 export class HelperService {
 	static extractPath(input: string): string {
 		const regex = /\[\[(.*?)(?:\|.*?)?\]\]/;
@@ -8,5 +10,12 @@ export class HelperService {
 		}
 
 		return undefined;
+	}
+
+	static getPositionInParent(elements: ElementInterface[]): number {
+		if (elements.length === 0) return 1;
+
+		const highestPositionInParent = Math.max(...elements.map((element: ElementInterface) => element.positionInParent));
+		return highestPositionInParent + 1;
 	}
 }
