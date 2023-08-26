7fe64af18381000d17e184b4ac4c898dc98f993a
Carlo Nicora <carlo.nicora@gmail.com>
2023-08-26 21:57:47 +0200

    feat: remove italian translation

--- src/services/InternationalisationService.ts
+++ src/services/InternationalisationService.ts
@@ -2,7 +2,6 @@ import i18n from "i18next";
 import I18nextBrowserLanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
 import { i18nEn } from "src/i18n/en";
-import { i18nIt } from "src/i18n/it";
 
 export class InternationalisationService {
 	static async loadSettings(): Promise<void> {
@@ -23,6 +22,5 @@ export class InternationalisationService {
 			});
 
 		i18n.addResourceBundle("en", "common", i18nEn);
-		i18n.addResourceBundle("it", "common", i18nIt);
 	}
 }
