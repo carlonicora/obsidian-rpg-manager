dce713f5f693b7afb0a5a2ce18fcbf4820b62988
Carlo Nicora <carlo.nicora@gmail.com>
2023-09-02 19:19:11 +0200

    feat: add link to documentation

--- src/components/options/OptionContainerComponent.tsx
+++ src/components/options/OptionContainerComponent.tsx
@@ -2,6 +2,7 @@ import { RpgManagerInterface } from "@/RpgManagerInterface";
 import { ElementInterface } from "@/data/interfaces/ElementInterface";
 import { useApi } from "@/hooks/useApi";
 import * as React from "react";
+import { useTranslation } from "react-i18next";
 import NewElementComponent from "../creation/NewElementComponent";
 import OptionsViewComponent from "./OptionsViewComponent";
 
@@ -10,7 +11,16 @@ export default function OptionContainerComponent({
 }: {
 	element: ElementInterface | undefined;
 }): React.ReactElement {
+	const { t } = useTranslation();
 	const api: RpgManagerInterface = useApi();
+
+	const showReadme = () => {
+		app.workspace.detachLeavesOfType("rpg-manager-readme");
+		app.workspace.getLeaf(true).setViewState({
+			type: "rpg-manager-readme",
+			active: true,
+		});
+	};
 
 	return (
 		<div className="flex flex-col min-h-screen pb-16">
@@ -21,8 +31,16 @@ export default function OptionContainerComponent({
 				</div>
 			)}
 			<NewElementComponent element={element} />
-			<div className="absolute bottom-8 left-0 right-0 pr-3 text-right text-[--text-faint] text-xs">
-				{"RPG Manager" + api.version}
+			<div className="rounded-lg border border-[--background-modifier-border] bg-[--background-primary] p-3 col-span-1 text-xs">
+				<div
+					className="cursor-pointer text-[--text-accent] hover:text-[--text-accent-hover] pl-2 pr-2 pt-1 pb-1 border border-transparent hover:bg-[--background-primary-alt] hover:border-[--background-modifier-border] rounded-lg"
+					onClick={showReadme}
+				>
+					{t("documentation")}
+				</div>
+				<div className="absolute bottom-8 left-0 right-0 pr-3 text-right text-[--text-faint] text-xs">
+					{"RPG Manager" + api.version}
+				</div>
 			</div>
 		</div>
 	);
