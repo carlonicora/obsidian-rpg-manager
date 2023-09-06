cf3cc6c7b2976ce3483d58c1a214a2d4eeabdd38
Carlo Nicora <carlo.nicora@gmail.com>
2023-09-06 12:50:16 +0200

    fix: correct default asset folder

--- src/services/FileUploadService.ts
+++ src/services/FileUploadService.ts
@@ -6,14 +6,25 @@ import { RpgManagerCodeblockService } from "src/services/RpgManagerCodeblockServ
 export class FileUploadService {
 	constructor(private _app: App, private _api: RpgManagerInterface) {}
 
-	uploadFileList(element: ElementInterface, files: FileList): Promise<void> {
+	async uploadFileList(element: ElementInterface, files: FileList): Promise<void> {
 		const codeblockService = new RpgManagerCodeblockService(this._app, this._api, element.file);
+
+		const folderName =
+			this._api.settings.assetsFolder !== undefined && this._api.settings.assetsFolder !== ""
+				? this._api.settings.assetsFolder
+				: "Assets";
+
+		if (!this._app.vault.getAbstractFileByPath(folderName)) {
+			try {
+				await this._app.vault.createFolder(folderName);
+			} catch (e) {
+				//no need to catch any error here
+			}
+		}
 
 		const availableFiles: TFile[] = this._app.vault
 			.getFiles()
-			.filter((file: TAbstractFile) =>
-				file.path.startsWith((this._api.settings.assetsFolder ?? "Assets") + "/" + element.name)
-			);
+			.filter((file: TAbstractFile) => file.path.startsWith(folderName + "/" + element.name));
 
 		const fs = require("fs");
 		const imagesToAdd: { path: string; caption: string }[] = [];
@@ -21,21 +32,9 @@ export class FileUploadService {
 		const copyPromises = Array.from(files).map((file: any, index: number) => {
 			const extension: string = file.name.substring(file.name.lastIndexOf("."));
 
-			const link: string =
-				(this._api.settings.assetsFolder === undefined || this._api.settings.assetsFolder === "" ? "Assets/" : "") +
-				element.name +
-				" " +
-				(availableFiles.length + index + 1) +
-				extension;
-			const finalPath =
-				(this._api.settings.assetsFolder ?? "Assets") +
-				"/" +
-				element.name +
-				" " +
-				(availableFiles.length + index + 1) +
-				extension;
+			const link: string = folderName + "/" + element.name + " " + (availableFiles.length + index + 1) + extension;
 
-			const path = this._app.vault.adapter.basePath + "/" + finalPath;
+			const path = this._app.vault.adapter.basePath + "/" + link;
 
 			imagesToAdd.push({ path: link, caption: "" });
 
