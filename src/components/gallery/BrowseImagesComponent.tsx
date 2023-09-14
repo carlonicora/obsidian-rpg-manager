67c04c46bde5af2b8b0f051995de7d54c53e053b
Carlo Nicora <carlo.nicora@gmail.com>
2023-09-14 22:37:59 +0200

    fix: correct image browing

--- src/components/gallery/BrowseImagesComponent.tsx
+++ src/components/gallery/BrowseImagesComponent.tsx
@@ -1,5 +1,5 @@
 import { useApp } from "@/hooks/useApp";
-import { App, TAbstractFile } from "obsidian";
+import { App, TAbstractFile, TFile } from "obsidian";
 import * as React from "react";
 import { RpgManagerInterface } from "src/RpgManagerInterface";
 import { ElementInterface } from "src/data/interfaces/ElementInterface";
@@ -18,9 +18,16 @@ export default function BrowseImagesComponent({
 	const api: RpgManagerInterface = useApi();
 	const app: App = useApp();
 
-	const allImages: TAbstractFile[] = app.vault
-		.getAllLoadedFiles()
-		.filter((file: TAbstractFile) => file.path.startsWith(api.settings.assetsFolder ?? "Assets"));
+	let allImages: TAbstractFile[] = app.vault.getAllLoadedFiles();
+
+	if (api.settings.assetsFolder !== undefined && api.settings.assetsFolder !== "") {
+		allImages = allImages.filter((file: TAbstractFile) => file.path.startsWith(api.settings.assetsFolder ?? "Assets"));
+	}
+
+	allImages = allImages.filter(
+		(file: TFile) =>
+			file.extension === "png" || file.extension === "jpg" || file.extension === "jpeg" || file.extension === "webp"
+	);
 
 	const handleAddExistingImage = async (image: ImageInterface) => {
 		const codeblockService = new RpgManagerCodeblockService(app, api, element.file);
@@ -42,7 +49,17 @@ export default function BrowseImagesComponent({
 			</div>
 			<div className="grid grid-cols-4 justify-center gap-3">
 				{allImages.map((imageFile: TAbstractFile, index: number) => {
-					const path = imageFile.path.substring((api.settings.assetsFolder ?? "Assets").length + 1);
+					let path;
+					if (
+						api.settings.assetsFolder !== undefined &&
+						api.settings.assetsFolder !== "" &&
+						imageFile.path.startsWith(api.settings.assetsFolder)
+					) {
+						path = imageFile.path.substring((api.settings.assetsFolder ?? "Assets").length + 1);
+					} else {
+						path = imageFile.path;
+					}
+
 					if (path.length === 0) return null;
 
 					const imageData = { path: path };
