1a072023eba80c74d89599b61f8d734cd9ad4ba9
Carlo Nicora <carlo.nicora@gmail.com>
2023-11-26 10:30:36 +0100

    refactor: changed scene design

--- src/components/elements/SceneComponent.tsx
+++ src/components/elements/SceneComponent.tsx
@@ -1,12 +1,15 @@
+import { AttributeType } from "@/data/enums/AttributeType";
 import * as React from "react";
 import { AttributeComponentType } from "src/data/enums/AttributeComponentType";
 import { AttributeInterface } from "src/data/interfaces/AttributeInterface";
 import { ElementInterface } from "src/data/interfaces/ElementInterface";
 import AttributeListComponent from "../attributes/AttributeListComponent";
+import DescriptionAttributeComponent from "../attributes/types/DescriptionAttributeComponent";
 import DurationAttributeComponent from "../attributes/types/DurationAttributeComponent";
-import ImageAndDescriptionComponent from "../groups/ImageAndDescriptionComponent";
+import ParentAttributeComponent from "../attributes/types/ParentAttributeComponent";
 import HeaderComponent from "../headers/HeaderComponent";
 import ImageCarouselComponent from "../images/ImageCarouselComponent";
+import ImageComponent from "../images/ImageComponent";
 import RelationshipsComponent from "../relationships/RelationshipsComponent";
 import TasksContainerComponent from "../tasks/TasksContainerComponent";
 
@@ -23,16 +26,21 @@ export default function SceneComponent({
 		<>
 			<div className="space-y-3 p-3 bg-[--background-primary-alt] border border-[--background-modifier-border]">
 				<HeaderComponent element={element} isInPopover={isInPopover} />
-				<div className={`gap-3 !mb-3 grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-5`}>
-					<div className={`lg:col-span-2`}>
-						<ImageAndDescriptionComponent
-							element={element}
-							isInPopover={isInPopover}
-							requiresImage={false}
-							showParent={true}
-						/>
+				{element.images.length > 0 && (
+					<div className="max-h-32 h-32 overflow-hidden">
+						<ImageComponent element={element} isEditable={!isInPopover} />
 					</div>
-					<div className={`lg:col-span-3`}>
+				)}
+				<div className={`grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-5 gap-3 !mb-3}`}>
+					<div className={`col-span-5 sm:col-span-1 lg:col-span-5`}>
+						<div className="rounded-lg border border-[--background-modifier-border] bg-[--background-primary] p-3 !mb-3">
+							<DescriptionAttributeComponent
+								element={element}
+								attribute={element.attribute(AttributeType.Description)}
+								isEditable={!isInPopover}
+							/>
+							<ParentAttributeComponent element={element} isEditable={!isInPopover} />
+						</div>
 						<AttributeListComponent element={element} isEditable={!isInPopover} />
 					</div>
 				</div>
