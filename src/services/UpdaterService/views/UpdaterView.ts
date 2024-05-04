7c3918f3bde0c4be15dd370dfd9f84a5474db9f0
Carlo Nicora <carlo.nicora@gmail.com>
2024-05-04 10:13:25 +0200

    wip: updater

--- src/services/UpdaterService/views/UpdaterView.ts
+++ src/services/UpdaterService/views/UpdaterView.ts
@@ -8,45 +8,52 @@ import { Root, createRoot } from "react-dom/client";
 import UpdaterComponent from "../components/UpdaterComponent";
 
 export class UpdaterView extends ItemView implements View {
-	protected viewType = "rpg-manager-updater";
-	protected displayText = "Update RPG Manager";
-	public icon = "d20";
-	private _element: ElementInterface | undefined;
-	private _root: Root | undefined = undefined;
+  protected viewType = "rpg-manager-updater";
+  protected displayText = "Update RPG Manager";
+  public icon = "d20";
+  private _element: ElementInterface | undefined;
+  private _root: Root | undefined = undefined;
 
-	constructor(private _app: App, private _api: RpgManagerInterface, leaf: WorkspaceLeaf) {
-		super(leaf);
+  constructor(
+    private _app: App,
+    private _api: RpgManagerInterface,
+    leaf: WorkspaceLeaf,
+  ) {
+    super(leaf);
 
-		this._root = createRoot(this.contentEl);
-	}
+    this._root = createRoot(this.contentEl);
+  }
 
-	getViewType(): string {
-		return this.viewType;
-	}
+  getViewType(): string {
+    return this.viewType;
+  }
 
-	onResize() {
-		super.onResize();
-		this.render();
-	}
+  onResize() {
+    super.onResize();
+    this.render();
+  }
 
-	getDisplayText(): string {
-		return this.displayText;
-	}
+  getDisplayText(): string {
+    return this.displayText;
+  }
 
-	protected async onOpen(): Promise<void> {
-		this.render();
-	}
+  protected async onOpen(): Promise<void> {
+    this.render();
+  }
 
-	async render(): Promise<void> {
-		const elementComponent = createElement(UpdaterComponent);
+  async render(): Promise<void> {
+    const elementComponent = createElement(UpdaterComponent);
 
-		const reactComponent = createElement(
-			AppContext.Provider,
-			{ value: this._app },
-			createElement(ApiContext.Provider, { value: this._api }, elementComponent)
-		);
-		//const reactComponent = createElement(ApiContext.Provider, { value: this._api }, elementComponent);
+    const reactComponent = createElement(
+      AppContext.Provider,
+      { value: this._app },
+      createElement(
+        ApiContext.Provider,
+        { value: this._api },
+        elementComponent,
+      ),
+    );
 
-		this._root.render(reactComponent);
-	}
+    this._root.render(reactComponent);
+  }
 }
